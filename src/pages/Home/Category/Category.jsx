bcd7434474c1fecd09b5aa76199210727210e169
Sakif Hridoy <sakifworkspace@gmail.com>
2024-01-07 13:53:55 +0600

    add swiper slide for food categories

---
+++ src/pages/Home/Category/Category.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+
+import img1 from "../../../assets/home/slide1.jpg"
+import img2 from "../../../assets/home/slide2.jpg"
+import img3 from "../../../assets/home/slide3.jpg"
+import img4 from "../../../assets/home/slide4.jpg"
+import img5 from "../../../assets/home/slide5.jpg"
+
+const Category = props => {
+    return (
+        <Swiper
+        slidesPerView={5}
+        spaceBetween={30}
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide><img src={img1} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={img2} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={img3} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={img4} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={img5} alt="" /></SwiperSlide>
+        
+      </Swiper>
+    );
+};
+
+Category.propTypes = {
+    
+};
+
+export default Category;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Banner from '../Banner/Banner';
+import Category from '../Category/Category';
 
 const Home = props => {
     return (
         <div>
             <Banner></Banner>
+            <Category></Category>
         </div>
     );
 };
