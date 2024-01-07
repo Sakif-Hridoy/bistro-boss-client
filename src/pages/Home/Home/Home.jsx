12283001f78cf4ccf407175004459cce2b44e281
Sakif Hridoy <sakifworkspace@gmail.com>
2024-01-07 12:59:12 +0600

    slider component,make autoplay infinityLoop

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Banner from '../Banner/Banner';
 
 const Home = props => {
     return (
         <div>
-            <h2>Home</h2>
+            <Banner></Banner>
         </div>
     );
 };
