f26addcdd3aa5ae5cca9fb495509726a0bcdd430
Sakif Hridoy <sakifworkspace@gmail.com>
2024-01-08 23:06:11 +0600

    display different menu items with different covers

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -3,15 +3,27 @@ import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import menuImg from '../../../assets/menu/banner3.jpg'
-
+import useMenu from '../../../hooks/useMenu';
+// import SectionTitle from '../../../components/SectionTitle'
+import MenuCategory from '../MenuCategory/MenuCategory';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import dessertImg from "../../../assets/menu/dessert-bg.jpeg"
 const Menu = props => {
+    const [menu] = useMenu()
+    const dessert = menu.filter(item=>item.category === 'dessert')
+    const salad = menu.filter(item=>item.category === 'salad')
+    const soup = menu.filter(item=>item.category === 'soup')
+    const pizza = menu.filter(item=>item.category === 'pizza')
+    const offered = menu.filter(item=>item.category === 'offered')
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
             <Cover img={menuImg} title="Our Menu"></Cover>
-            
+            <SectionTitle subHeading="Dont Miss" heading="Todays Offer"></SectionTitle>
+            <MenuCategory items={offered} title="Offered Items"></MenuCategory>
+            <MenuCategory items={dessert} title="Desserts" img={dessertImg}></MenuCategory>
         </div>
     );
 };
--- src/pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import MenuItem from '../../Shared/MenuItem/MenuItem';
+import Cover from '../../Shared/Cover/Cover';
 
-const MenuCategory = props => {
+const MenuCategory = ({items,title,img}) => {
     return (
         <div>
-            
+            {title && <Cover img={img} title={title}></Cover>}
+              <div className='grid md:grid-cols-2 gap-4'>
+            {
+                items.map(item=><MenuItem key={item._id} item={item}></MenuItem>)
+            }
+        </div>
         </div>
     );
 };
