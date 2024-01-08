4e49091c6b3aa759a63cf707ac1ae00a5896f5af
Sakif Hridoy <sakifworkspace@gmail.com>
2024-01-08 21:29:56 +0600

    react parallax

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from '../../../assets/menu/banner3.jpg'
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
 
 const Menu = props => {
     return (
@@ -8,7 +11,13 @@ const Menu = props => {
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
-            <h2>Menu</h2>
+            <Cover img={menuImg} title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={menuImg} title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={menuImg} title="Our Menu"></Cover>
+            <PopularMenu></PopularMenu>
+
         </div>
     );
 };
